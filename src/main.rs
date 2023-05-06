23863e16a3ec583d59e27932e0bceea8bf23ec7e
Leah Bowen <leahbowie@outlook.com>
2023-05-06 14:08:21 -0600

    bevy setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::{prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3dBundle::default());
 }
